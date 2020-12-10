58c3704439ebbf5701422f13ef2b8330f364429b
rogerho <rogerho@umich.edu>
2020-12-10 14:16:09 +0800

    framework - color schemes

---
+++ src/components/header.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+
+class Header extends React.Component {
+    render() {
+        return(
+            <Navbar>
+                eee
+            </Navbar>
+        )
+    }
+}
+
+export default Header
